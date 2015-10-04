3d055aee0bbb86166d2d5c19ceb207fd0838ad9f
Nate H. <nhawk@zephyrsports.com>
2015-10-04 13:05:39 -0700

    Initial Files

---
+++ led.php
@@ -0,0 +1,14 @@
+<?php
+$onoroff = $_GET["state"]; // Declares the request from index.html as a variable
+$textfile = "LEDstate.txt"; // Declares the name and location of the .txt file
+
+$fileLocation = "$textfile";
+$fh = fopen($fileLocation, 'w   ') or die("Something went wrong!"); // Opens up the .txt file for writing and replaces any previous content
+$stringToWrite = "$onoroff"; // Write either 1 or 0 depending on request from index.html
+fwrite($fh, $stringToWrite); // Writes it to the .txt file
+usleep(50000);
+fwrite($fh, 0);
+fclose($fh);
+
+header("Location: index.html"); // Return to frontend (index.html)
+?>
